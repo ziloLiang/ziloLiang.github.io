40131548b8515cd3c8df8b3b677b9e46fb631adf
ziloLiang <liangdong891228@126.com>
2017-10-29 15:09:41 +0800

    10月29日更新

--- src/json/juven.js
+++ src/json/juven.js
@@ -4,7 +4,7 @@ export default [
     {id: 3, name: 'Jacky Wu', head: 'images/jackyWu.jpeg'},
     {id: 4, name: 'God Loves You', head: 'images/godlovesyou.jpeg'},
     {id: 5, name: '杨洋', head: 'images/yangyang.jpeg'},
-    {id: 6, name: 'Juve1997照着点小老弟我', head: 'images/1997xiaolaodi.jpeg'},
+    {id: 6, name: 'Juve1997大王叫我来巡山', head: 'images/1997xiaolaodi.jpeg'},
     {id: 7, name: '2018新米屌启航 Fozza Merda', head: 'images/xinmidiaoqihang.jpeg'},
     {id: 8, name: 'Alan35', head: 'images/alan35.jpeg'},
     {id: 9, name: 'ice', head: 'images/ice.jpg'},
