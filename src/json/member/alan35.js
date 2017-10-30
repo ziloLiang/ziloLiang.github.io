3a96d0b5af80060a1790258a0ba74724a64c267e
ziloLiang <liangdong891228@126.com>
2017-10-30 13:45:52 +0800

    10月29日数据错误

--- src/json/member/alan35.js
+++ src/json/member/alan35.js
@@ -3,6 +3,6 @@ export default {
     name: 'Alan35',
     scoreList: [
         {match: '#927 尤文 2:0 奥林匹亚科斯', score: 1},
-        {match: '#1029 AC米兰 2:0 尤文图斯', score: 1}
+        {match: '#1029 AC米兰 0:2 尤文图斯', score: 1}
     ]
 }
