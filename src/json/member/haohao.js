4090a6a57449dff9dd600bd1d88ae67f6d5884d4
liangdong06 <liangdong06@baidu.com>
2018-04-09 12:29:00 +0800

    update data

--- src/json/member/haohao.js
+++ src/json/member/haohao.js
@@ -2,6 +2,7 @@ export default {
     id: 30,
     name: '昊昊',
     scoreList: [
-        {match: '#180315 尤文 2:0 亚特兰大', score: 1}
+        {match: '#180315 尤文 2:0 亚特兰大', score: 1},
+        {match: '#180404 尤文 0:3 皇马', score: 1},
     ]
 }
--- src/json/member/xiaolv.js
+++ src/json/member/xiaolv.js
@@ -3,6 +3,7 @@ export default {
     name: '小绿',
     scoreList: [
         {match: '#11.23 尤文 0:0 巴塞罗那', score: 1},
-        {match: '#12.10 尤文 0:0 国际米兰', score: 1}
+        {match: '#12.10 尤文 0:0 国际米兰', score: 1},
+        {match: '#180318 斯帕尔 0:0 尤文图斯', score: 1}
     ]
 }
