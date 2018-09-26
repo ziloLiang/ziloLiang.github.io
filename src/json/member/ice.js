4d0c6e06f67e4fe2d897bc3d7427e62db5fca0dd
liangdong06 <liangdong06@baidu.com>
2018-09-26 10:55:10 +0800

    update

--- src/json/member/ice.js
+++ src/json/member/ice.js
@@ -2,5 +2,6 @@ export default {
     id: 9,
     name: 'ice',
     scoreList: [
+        {match: '20180924 弗洛西诺内0:2尤文', score: 1}
     ]
 };
--- src/json/member/luotuoxiangzi.js
+++ src/json/member/luotuoxiangzi.js
@@ -2,5 +2,6 @@ export default {
     id: 23,
     name: '骆驼祥子',
     scoreList: [
+        {match: '20180924 弗洛西诺内0:2尤文', score: 1}
     ]
 }
