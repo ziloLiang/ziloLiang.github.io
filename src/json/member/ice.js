fbac85d9c248c3ae847e268e51646c640adb736c
liangdong06 <liangdong06@baidu.com>
2018-12-19 18:49:50 +0800

    update

--- src/json/member/ice.js
+++ src/json/member/ice.js
@@ -5,5 +5,6 @@ export default {
         {match: '20180924 弗洛西诺内0:2尤文', score: 1},
         {match: '20180930 尤文3:1那不勒斯', score: 1},
         {match: '20181125 尤文2:0斯帕尔', score: 1},
+        {match: '20181216 都灵0:1尤文', score: 1},
     ]
 };
--- src/json/member/ye.js
+++ src/json/member/ye.js
@@ -2,5 +2,6 @@ export default {
     id: 16,
     name: 'ye',
     scoreList: [
+        {match: '20181216 都灵0:1尤文', score: 1},
     ]
 }
