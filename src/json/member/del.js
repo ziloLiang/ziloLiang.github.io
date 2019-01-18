5b5a14af404f605c437ccdf8be79d415b8185c34
liangdong06 <liangdong06@baidu.com>
2019-01-18 15:38:57 +0800

    update data

--- src/json/member/del.js
+++ src/json/member/del.js
@@ -2,5 +2,6 @@ export default {
     id: 18,
     name: 'Del',
     scoreList: [
+        {match: '20190117 尤文1:0AC米兰', score: 1},
     ]
 };
--- src/json/member/ye.js
+++ src/json/member/ye.js
@@ -5,5 +5,6 @@ export default {
         {match: '20181216 都灵0:1尤文', score: 1},
         {match: '20181223 尤文1:0罗马', score: 1},
         {match: '20181223 尤文2:1桑普', score: 1},
+        {match: '20190117 尤文1:0AC米兰', score: 1},
     ]
 }
